cba63a2930f10f2ee6d7c06c92590c84ae976539
Natha0b <laura.barona04@gmail.com>
2022-12-16 11:58:24 -0500

    create function strtok

---
+++ func_strtok.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+/**
+ * main - function main
+ * @void: variable void
+ * Return: int
+ */
+
+int main(void)
+{
+	char *arg[100], *str_p = getenv("PATH");
+	char str[100], *len = str, *n = NULL;
+
+	while (str_p[i])
+		{
+			i++;
+		}
+
+		if (strcmp(buff, "PATH") == 0)
+		{
+
+			strcpy(str, string_path);
+			while ((n = strtok(len, ":")) != NULL)
+			{
+				printf("%s\n", n);
+				n = NULL;
+			}
+		}
+}
