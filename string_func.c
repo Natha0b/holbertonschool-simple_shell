1c2059d6295a00db6544b0595b092a0500b98d0b
Natha0b <laura.barona04@gmail.com>
2022-12-19 11:53:35 -0500

    add function strcat

--- string_func.c
+++ string_func.c
@@ -108,3 +108,30 @@ char *_getenv(const char *name)
 	}
 	return (NULL);
 }
+
+/**
+ **_strcat - function a string
+ *@dest: varible punter
+ *@src: varible punter
+ *Return: pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *p = dest;
+	int c;
+	int n;
+	
+	for (c = 0; dest[c]; c++)
+	{
+	
+	}
+	
+	for (n = 0; src[n]; n++, c++)
+	{
+		dest[c] = src[n];
+	}
+	
+	dest[c] = '\0';
+	return (p);
+
+}
