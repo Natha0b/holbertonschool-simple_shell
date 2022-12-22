803ffb9af0bedfa103e524684612a653cee32366
andres98100 <andressolarte580@gmail.com>
2022-12-22 18:28:48 -0500

    finish proyect

--- cmd.c
+++ cmd.c
@@ -3,7 +3,6 @@
 /**
  * execute_env - function execute the process
  * @arg: variable char
- * @environ: variable char
  * Return: void
 */
 
@@ -12,14 +11,10 @@ int execute_env(char **arg)
 	pid_t p_id;
 	int status;
 
-	/* we call path and pass the first command to it */
 	arg[0] = search_path(arg[0]);
 	if (arg[0] != NULL)
 	{
-		/* we create the child process */
 		p_id = fork();
-		/* if the child process fails we print an error*/
-		/*if the command is not executed. */
 		if (p_id == 0)
 		{
 			if (execve(arg[0], arg, environ) == -1)
@@ -29,13 +24,11 @@ int execute_env(char **arg)
 		}
 		else
 		{
-			/* we expect the son process to die */
 			wait(&status);
 			if (WIFEXITED(status))
 				status = WEXITSTATUS(status);
 		}
 	}
-	/* if the child process fails we print error */
 	else
 		perror("Error:");
 
